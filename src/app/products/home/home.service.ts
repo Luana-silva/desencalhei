cd6d2ba3c246dbd1df55e8853471112255009411
Luana silva <luana-silva@gmail.com>
2018-09-06 17:31:38 -0300

    Mais atualizações

--- src/app/products/home/home.service.ts
+++ src/app/products/home/home.service.ts
@@ -13,20 +13,19 @@ export class HomeService {
   constructor(private http: Http) { }
 
   bannersteste(): Observable<any> {
-    // http://192.168.123.10:8080/DesencalheiWs/rs/banner/bannerImage/a8e68da2-304e-44ac-8e44-efcdd4f2c8a1/DESKTOP
     return this.http.get(`${SERVICE_URL}/DesencalheiWs/rs/banner/listAll`)
       .map(response => response.json())
       .map(response => response.data)
   }
 
-  banners(tipo): Observable<any> {
+  banners(type): Observable<any> {
     const headers = new Headers();
     headers.append('Access-Control-Allow-Headers', 'Content-Type');
     headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     headers.append('Access-Control-Allow-Origin', '*');
     headers.append('Content-Type', 'application/json')
 
-    return this.http.post(`${SERVICE_URL}/DesencalheiWs/rs/banner/search`, tipo,
+    return this.http.post(`${SERVICE_URL}/DesencalheiWs/rs/banner/search`, type,
       new RequestOptions({headers: headers}))
       .map(response => response.json())
       .map(response => response.data)
